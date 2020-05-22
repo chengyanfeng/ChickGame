d193d572286351044e028bc43d20117344b97a4a
yanfeng <chengyanfengchengyanfeng@genyuanlian.com>
2020-05-22 10:04:39 +0800

    测试

--- main.go
+++ main.go
@@ -11,9 +11,7 @@ func init(){
 }
 func main() {
 	fmt.a
-	fmt.p
-	a
-	.pra
+	fmt.
 	beego.SetStaticPath("/MP_verify_oSClQLOUTyzPRg6o.txt","MP_verify_oSClQLOUTyzPRg6o.txt")
 	def.Outtradeno=beego.AppConfig.String("outtradeno")
 	beego.Run()
